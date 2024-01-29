864f0321752556b05cffe7f6bd6a21887af7321e
Ferdinand Calo <fexcalo@gmail.com>
2024-01-29 23:18:06 +0100

    Update UniReg.java

--- src/main/java/net/fexcraft/mod/fcl/UniReg.java
+++ src/main/java/net/fexcraft/mod/fcl/UniReg.java
@@ -14,13 +14,13 @@ public class UniReg {
 	public static LinkedHashMap<String, Class<? extends ContainerInterface>> MENU = new LinkedHashMap<>();
 	public static LinkedHashMap<String, String> MENU_JSON = new LinkedHashMap<>();
 
-	public static boolean registerUI(String key, Class<UserInterface> ui){
+	public static boolean registerUI(String key, Class<? extends UserInterface> ui){
 		if(GUI.containsKey(key)) return false;
 		GUI.put(key, ui);
 		return true;
 	}
 
-	public static boolean registerMenu(String key, String loc, Class<ContainerInterface> con){
+	public static boolean registerMenu(String key, String loc, Class<? extends ContainerInterface> con){
 		if(MENU.containsKey(key)) return false;
 		MENU.put(key, con);
 		MENU_JSON.put(key, loc);
