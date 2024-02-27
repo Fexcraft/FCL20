9f87695f69ea8c57de0b67f366c1c60010f0325d
Ferdinand Calo <fexcalo@gmail.com>
2024-02-27 14:11:20 +0100

    Update UniversalAttachments.java

--- src/main/java/net/fexcraft/mod/fcl/UniversalAttachments.java
+++ src/main/java/net/fexcraft/mod/fcl/UniversalAttachments.java
@@ -1,13 +1,13 @@
 package net.fexcraft.mod.fcl;
 
-import net.fexcraft.mod.fcl.util.Passenger;
+import net.fexcraft.mod.fcl.util.PassImpl;
+import net.fexcraft.mod.uni.world.EntityW;
 import net.neoforged.bus.api.IEventBus;
 import net.neoforged.neoforge.attachment.AttachmentType;
 import net.neoforged.neoforge.attachment.IAttachmentHolder;
 import net.neoforged.neoforge.registries.DeferredRegister;
 import net.neoforged.neoforge.registries.NeoForgeRegistries;
 
-import java.lang.reflect.InvocationTargetException;
 import java.util.function.Supplier;
 
 /**
@@ -16,15 +16,15 @@ import java.util.function.Supplier;
 public class UniversalAttachments {
 
 	private static final DeferredRegister<AttachmentType<?>> ATTACHMENT_TYPES = DeferredRegister.create(NeoForgeRegistries.Keys.ATTACHMENT_TYPES, "fsmm");
-	public static final Class<? extends Passenger> PASS_IMPL = Passenger.class;
-	public static final Supplier<AttachmentType<Passenger>> PASSENGER = ATTACHMENT_TYPES
+	public static final Class<? extends EntityW> PASS_IMPL = PassImpl.class;
+	public static final Supplier<AttachmentType<EntityW>> PASSENGER = ATTACHMENT_TYPES
 		.register("passenger", () -> AttachmentType.builder(iah -> {
 			try{
 				return PASS_IMPL.getConstructor(IAttachmentHolder.class).newInstance(iah);
 			}
 			catch(Exception e){
 				e.printStackTrace();
-				return new Passenger(iah);
+				return new PassImpl(iah);
 			}
 		}).build());
 
