bd19097262871ebfd77c14a1d2f4f7e7ef2479dd
Ferdinand Calo <fexcalo@gmail.com>
2024-03-03 14:07:46 +0100

    Update SWI.java

--- src/main/java/net/fexcraft/mod/uni/impl/SWI.java
+++ src/main/java/net/fexcraft/mod/uni/impl/SWI.java
@@ -3,6 +3,7 @@ package net.fexcraft.mod.uni.impl;
 import net.fexcraft.mod.uni.item.ItemWrapper;
 import net.fexcraft.mod.uni.item.StackWrapper;
 import net.fexcraft.mod.uni.tag.TagCW;
+import net.minecraft.nbt.CompoundTag;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.ItemLike;
 
@@ -42,6 +43,51 @@ public class SWI extends StackWrapper {
 	@Override
 	public boolean hasTag(){
 		return stack.hasTag();
+	}
+
+	@Override
+	public String getName(){
+		return stack.getDisplayName().getString();
+	}
+
+	@Override
+	public int maxsize(){
+		return stack.getMaxStackSize();
+	}
+
+	@Override
+	public int damage(){
+		return stack.getDamageValue();
+	}
+
+	@Override
+	public int count(){
+		return stack.getCount();
+	}
+
+	@Override
+	public void count(int am){
+		stack.setCount(am);
+	}
+
+	@Override
+	public StackWrapper copy(){
+		return new SWI(stack.copy());
+	}
+
+	@Override
+	public void save(TagCW com){
+		stack.save(com.local());
+	}
+
+	@Override
+	public boolean empty(){
+		return stack.isEmpty();
+	}
+
+	@Override
+	public void createTagIfMissing(){
+		if(!stack.hasTag()) stack.setTag(new CompoundTag());
 	}
 
 }
