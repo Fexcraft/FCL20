23b593b4800791b05539667f2f85833b7762cebf
Ferdinand Calo <fexcalo@gmail.com>
2024-03-24 13:40:46 +0100

    Update FCL.java

--- neo20/src/main/java/net/fexcraft/mod/fcl/FCL.java
+++ neo20/src/main/java/net/fexcraft/mod/fcl/FCL.java
@@ -1,12 +1,17 @@
 package net.fexcraft.mod.fcl;
 
 import com.mojang.logging.LogUtils;
-import net.fexcraft.mod.fcl.util.PassengerGetter;
+import net.fexcraft.mod.fcl.util.PassengerUtil;
 import net.fexcraft.mod.fcl.util.UIPacketN;
 import net.fexcraft.mod.uni.ui.ContainerInterface;
 import net.fexcraft.mod.uni.uimpl.*;
 import net.minecraft.core.registries.Registries;
+import net.minecraft.network.chat.Component;
 import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.MenuProvider;
+import net.minecraft.world.entity.player.Inventory;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.inventory.AbstractContainerMenu;
 import net.minecraft.world.inventory.MenuType;
 import net.neoforged.bus.api.IEventBus;
 import net.neoforged.bus.api.SubscribeEvent;
@@ -19,6 +24,7 @@ import net.neoforged.neoforge.network.event.RegisterPayloadHandlerEvent;
 import net.neoforged.neoforge.network.registration.IPayloadRegistrar;
 import net.neoforged.neoforge.registries.DeferredHolder;
 import net.neoforged.neoforge.registries.DeferredRegister;
+import org.jetbrains.annotations.Nullable;
 import org.slf4j.Logger;
 
 /**
@@ -34,7 +40,27 @@ public class FCL {
 	public static final DeferredHolder<MenuType<?>, MenuType<UniCon>> UNIVERSAL = CONTAINERS.register("editor", () -> IMenuTypeExtension.create(UniCon::new));
 
 	public FCL(IEventBus bus){
-		PassengerGetter.GETTER = entity -> entity.getData(UniversalAttachments.PASSENGER);
+		PassengerUtil.GETTER = entity -> entity.getData(UniversalAttachments.PASSENGER);
+		PassengerUtil.UI_OPENER = (player, ui, pos) -> {
+			player.openMenu(new MenuProvider() {
+				@Override
+				public Component getDisplayName(){
+					return Component.literal("Fexcraft Universal UI");
+				}
+
+				@Nullable
+				@Override
+				public AbstractContainerMenu createMenu(int i, Inventory inventory, Player player){
+					return new UniCon(i, inventory, ui, null, pos);
+				}
+			},buf -> {
+				buf.writeInt(ui.length());
+				buf.writeUtf(ui);
+				buf.writeInt(pos.x);
+				buf.writeInt(pos.y);
+				buf.writeInt(pos.z);
+			});
+		};
 		FCL20.init(!FMLEnvironment.production, FMLLoader.getDist().isClient());
 		ContainerInterface.SEND_TO_CLIENT = (com, player) -> PacketDistributor.PLAYER.with(player.local()).send(new UIPacketN(com.local()));
 		ContainerInterface.SEND_TO_SERVER = com -> PacketDistributor.SERVER.noArg().send(new UIPacketN(com.local()));
