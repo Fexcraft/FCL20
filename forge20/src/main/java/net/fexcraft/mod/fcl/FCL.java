480108138e449e92005a201f0e5d2d599c5e2765
Ferdinand Calo <fexcalo@gmail.com>
2024-03-24 13:40:33 +0100

    Update FCL.java

--- forge20/src/main/java/net/fexcraft/mod/fcl/FCL.java
+++ forge20/src/main/java/net/fexcraft/mod/fcl/FCL.java
@@ -1,21 +1,33 @@
 package net.fexcraft.mod.fcl;
 
 import com.mojang.logging.LogUtils;
-import net.fexcraft.mod.fcl.util.PassengerGetter;
+import net.fexcraft.mod.fcl.util.PassProvider;
+import net.fexcraft.mod.fcl.util.PassengerUtil;
 import net.fexcraft.mod.fcl.util.UIPacketF;
 import net.fexcraft.mod.uni.ui.ContainerInterface;
 import net.fexcraft.mod.uni.uimpl.UniCon;
 import net.minecraft.core.registries.Registries;
+import net.minecraft.network.chat.Component;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.server.level.ServerPlayer;
+import net.minecraft.world.MenuProvider;
+import net.minecraft.world.entity.Entity;
+import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.entity.player.Inventory;
 import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.inventory.AbstractContainerMenu;
 import net.minecraft.world.inventory.MenuType;
+import net.minecraftforge.common.capabilities.RegisterCapabilitiesEvent;
 import net.minecraftforge.common.extensions.IForgeMenuType;
+import net.minecraftforge.event.AttachCapabilitiesEvent;
 import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import net.minecraftforge.fml.loading.FMLEnvironment;
 import net.minecraftforge.fml.loading.FMLLoader;
+import net.minecraftforge.network.NetworkHooks;
 import net.minecraftforge.network.NetworkRegistry;
 import net.minecraftforge.network.PacketDistributor;
 import net.minecraftforge.network.simple.SimpleChannel;
@@ -33,19 +45,39 @@ public class FCL {
 	public static final Logger LOGGER = LogUtils.getLogger();
 	public static final ResourceLocation UI_PACKET = new ResourceLocation(MODID, "ui");
 	public static final DeferredRegister<MenuType<?>> CONTAINERS = DeferredRegister.create(Registries.MENU, MODID);
-	public static final RegistryObject<MenuType<UniCon>> UNIVERSAL = CONTAINERS.register("editor", () -> IForgeMenuType.create(UniCon::new));
+	public static final RegistryObject<MenuType<UniCon>> UNIVERSAL = CONTAINERS.register("universal", () -> IForgeMenuType.create(UniCon::new));
 	public static final SimpleChannel CHANNEL = NetworkRegistry.ChannelBuilder.named(new ResourceLocation("fcl", "channel"))
 		.clientAcceptedVersions(pro -> true)
 		.serverAcceptedVersions(pro -> true)
 		.networkProtocolVersion(() -> "fcl")
 		.simpleChannel();
 
-	public FCL(IEventBus bus){
+	public FCL(){
+		IEventBus bus = FMLJavaModLoadingContext.get().getModEventBus();
 		FCL20.init(!FMLEnvironment.production, FMLLoader.getDist().isClient());
-		PassengerGetter.GETTER = entity -> null;//TODO
+		PassengerUtil.GETTER = entity -> entity.getCapability(PassProvider.CAPABILITY).resolve().get();
+		PassengerUtil.UI_OPENER = (player, ui, pos) -> {
+			NetworkHooks.openScreen((ServerPlayer)player, new MenuProvider() {
+				@Override
+				public Component getDisplayName(){
+					return Component.literal("Fexcraft Universal UI");
+				}
+
+				@Override
+				public AbstractContainerMenu createMenu(int i, Inventory inventory, Player player){
+					return new UniCon(i, inventory, ui, null, pos);
+				}
+			}, buf -> {
+				buf.writeInt(ui.length());
+				buf.writeUtf(ui);
+				buf.writeInt(pos.x);
+				buf.writeInt(pos.y);
+				buf.writeInt(pos.z);
+			});
+		};
 		//
-		bus.addListener(this::commonSetup);
 		CONTAINERS.register(bus);
+		bus.addListener(this::commonSetup);
 	}
 
 	private void commonSetup(FMLCommonSetupEvent event){
@@ -64,6 +96,22 @@ public class FCL {
 		});
 		ContainerInterface.SEND_TO_CLIENT = (com, player) -> CHANNEL.send(PacketDistributor.PLAYER.with(player.local()), new UIPacketF(com.local()));
 		ContainerInterface.SEND_TO_SERVER = com -> CHANNEL.sendToServer(new UIPacketF(com.local()));
+	}
+
+	@Mod.EventBusSubscriber(modid = "fvtm", bus = Mod.EventBusSubscriber.Bus.MOD)
+	public static class Events {
+
+		@SubscribeEvent
+		public void registerCaps(RegisterCapabilitiesEvent event){
+			event.register(PassengerUtil.PASS_IMPL);
+		}
+
+		@SubscribeEvent
+		public static void onClientSetup(AttachCapabilitiesEvent<Entity> event){
+			if(event.getObject() instanceof LivingEntity){
+				event.addCapability(new ResourceLocation("fcl:passenger"), new PassProvider(event.getObject()));
+			}
+		}
 	}
 
 }
