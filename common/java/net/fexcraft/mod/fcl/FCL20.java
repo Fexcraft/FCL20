a92b54436815228347d27e39103ecf70ef3b4438
Ferdinand Calo <fexcalo@gmail.com>
2024-07-07 17:51:58 +0200

    Update FCL20.java

--- common/java/net/fexcraft/mod/fcl/FCL20.java
+++ common/java/net/fexcraft/mod/fcl/FCL20.java
@@ -2,8 +2,10 @@ package net.fexcraft.mod.fcl;
 
 import net.fexcraft.lib.common.math.AxisRotator;
 import net.fexcraft.mod.fcl.util.Axis3DL;
+import net.fexcraft.mod.fcl.util.EntityUtil;
 import net.fexcraft.mod.uni.EnvInfo;
 import net.fexcraft.mod.uni.IDLManager;
+import net.fexcraft.mod.uni.UniEntity;
 import net.fexcraft.mod.uni.UniReg;
 import net.fexcraft.mod.uni.impl.*;
 import net.fexcraft.mod.uni.item.ItemWrapper;
@@ -19,8 +21,11 @@ import net.fexcraft.mod.uni.world.StateWrapper;
 import net.minecraft.client.resources.language.I18n;
 import net.minecraft.core.BlockPos;
 import net.minecraft.core.Direction;
+import net.minecraft.core.registries.BuiltInRegistries;
 import net.minecraft.nbt.CompoundTag;
+import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.InteractionHand;
+import net.minecraft.world.entity.Entity;
 import net.minecraft.world.item.BlockItem;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
@@ -47,6 +52,8 @@ public class FCL20 {
 		TagCW.WRAPPER[0] = com -> new TagCWI((CompoundTag)com);
 		TagCW.SUPPLIER[0] = () -> new TagCWI();
 		TagLW.SUPPLIER[0] = () -> new TagLWI();
+		ItemWrapper.GETTER = id -> BuiltInRegistries.ITEM.get(new ResourceLocation(id));
+		ItemWrapper.SUPPLIER = item -> new IWI((Item)item);
 		StackWrapper.SUPPLIER = obj -> {
 			if(obj instanceof ItemWrapper) return new SWI((ItemWrapper)obj);
 			if(obj instanceof ItemStack) return new SWI((ItemStack)obj);
@@ -65,6 +72,7 @@ public class FCL20 {
 			}
 			else return StateWrapper.DEFAULT;
 		};
+		UniEntity.ENTITY_GETTER = ent -> EntityUtil.get((Entity)ent);
 		if(EnvInfo.CLIENT){
 			UITab.IMPLEMENTATION = UUITab.class;
 			UIText.IMPLEMENTATION = UUIText.class;
