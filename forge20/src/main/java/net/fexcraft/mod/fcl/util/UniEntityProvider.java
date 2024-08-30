ddece30dbc400f6b5cff5359cf79b73f76be289d
Ferdinand Calo <fexcalo@gmail.com>
2024-08-30 18:50:35 +0200

    Update UniEntityProvider.java

--- forge20/src/main/java/net/fexcraft/mod/fcl/util/UniEntityProvider.java
+++ forge20/src/main/java/net/fexcraft/mod/fcl/util/UniEntityProvider.java
@@ -1,12 +1,11 @@
 package net.fexcraft.mod.fcl.util;
 
 import net.fexcraft.mod.uni.UniEntity;
+import net.fexcraft.mod.uni.tag.TagCW;
 import net.minecraft.core.Direction;
+import net.minecraft.nbt.CompoundTag;
 import net.minecraft.world.entity.Entity;
-import net.minecraftforge.common.capabilities.Capability;
-import net.minecraftforge.common.capabilities.CapabilityManager;
-import net.minecraftforge.common.capabilities.CapabilityToken;
-import net.minecraftforge.common.capabilities.ICapabilityProvider;
+import net.minecraftforge.common.capabilities.*;
 import net.minecraftforge.common.util.LazyOptional;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
@@ -14,7 +13,7 @@ import org.jetbrains.annotations.Nullable;
 /**
  * @author Ferdinand Calo' (FEX___96)
  */
-public class UniEntityProvider implements ICapabilityProvider {
+public class UniEntityProvider implements ICapabilitySerializable<CompoundTag> {
 
 	public static final Capability<UniEntity> CAPABILITY = CapabilityManager.get(new CapabilityToken<>(){});
 	private LazyOptional<UniEntity> optional;
@@ -28,6 +27,19 @@ public class UniEntityProvider implements ICapabilityProvider {
 	@Override
 	public @NotNull <T> LazyOptional<T> getCapability(@NotNull Capability<T> capability, @Nullable Direction direction){
 		return capability == CAPABILITY ? optional.cast() : LazyOptional.empty();
+	}
+
+	@Override
+	public CompoundTag serializeNBT(){
+		TagCW com = TagCW.create();
+		unient.appended.save(com);
+		return com.local();
+	}
+
+	@Override
+	public void deserializeNBT(CompoundTag com){
+		if(com == null) return;
+		unient.appended.load(TagCW.wrap(com));
 	}
 
 }
