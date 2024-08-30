be1226c81534a4b128c9a6c3017bca5cf38bb612
Ferdinand Calo <fexcalo@gmail.com>
2024-08-30 18:13:56 +0200

    Update ClientEvents.java

--- forge20/src/main/java/net/fexcraft/mod/fcl/ClientEvents.java
+++ forge20/src/main/java/net/fexcraft/mod/fcl/ClientEvents.java
@@ -1,6 +1,6 @@
 package net.fexcraft.mod.fcl;
 
-import net.fexcraft.mod.uni.uimpl.UniUI;
+import net.fexcraft.mod.uni.ui.UniUI;
 import net.minecraft.client.gui.screens.MenuScreens;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
